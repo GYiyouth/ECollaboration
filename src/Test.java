661df8174d7981930c2eed22808f619c44dc498a
葛尧 <geyao2012@gmail.com>
2016-11-29 15:12:41 +0800

    1

--- src/Test.java
+++ src/Test.java
@@ -37,7 +37,7 @@ public class Test {
 //		ECFileBean ecFileBean = null;
 //		try {
 //
-//发生的发生324234沃尔沃二
+//发生的
 //			ecFileBean = ecFileDAO.getFileInfo(2);
 //			ecFileBean.setFileName("photo");
 //			int id = ecFileDAO.addPhoto(ecFileBean, 2);
