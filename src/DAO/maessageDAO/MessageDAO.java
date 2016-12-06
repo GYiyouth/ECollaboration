b465a1d28d827b43f076023dfc295c1fbbc589ac
GengRui <gr2016@mail.ustc.edu.cn>
2016-12-06 17:56:28 +0800

    bean DAO DAOImpl 补充一下

--- src/DAO/maessageDAO/MessageDAO.java
+++ src/DAO/maessageDAO/MessageDAO.java
@@ -5,6 +5,7 @@ import bean.domain.UserBean;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by GR on 2016/12/5.
@@ -13,10 +14,18 @@ public interface MessageDAO {
     /**
      * 添加消息，内部生成id，返回消息id
      * @param messageBean
-     * @return int
+     * @return messageId
      * @throws SQLException
      */
-    public int addMessage(MessageBean messageBean) throws SQLException;
+    public Integer addMessage(MessageBean messageBean) throws SQLException;
+
+    /**
+     * 添加到 消息-收件人 的关系表中，
+     * @param messageId,receiverId
+     * @return boolean
+     * @throws SQLException
+     */
+    public boolean addMessageReceiver(int messageId,List<Integer> receiverIds) throws SQLException;
 
     /**
      * 获取MessageBean，通过messageId
@@ -25,16 +34,6 @@ public interface MessageDAO {
      * @throws SQLException
      */
     public MessageBean getMessageInfo(int messageId) throws SQLException;
-
-    /**
-     * 删除消息，获取MessageBean，通过messageId
-     * @param messageId
-     * @return MessageBean
-     * @throws SQLException
-     */
-
-
-    public MessageBean deleteByMessageId(int messageId) throws SQLException;
 
     /**
      * 判断该消息是否已经被所有的人阅读，，通过messageId
@@ -52,7 +51,7 @@ public interface MessageDAO {
      * @throws SQLException
      */
 
-    public int getSenderIdByMessageId(int messageId) throws SQLException;
+    public Integer getSenderIdByMessageId(int messageId) throws SQLException;
 
     /**
      * 获取发送者UserBean，通过messageId
--- src/DAO/maessageDAO/MessageDAOImpl.java
+++ src/DAO/maessageDAO/MessageDAOImpl.java
@@ -1,26 +1,100 @@
 package DAO.maessageDAO;
 
+import DAO.com.util.db.DBUtils;
 import bean.domain.MessageBean;
 import bean.domain.UserBean;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by GR on 2016/12/5.
  */
-public class MessageDAOImpl implements MessageDAO{
+public class MessageDAOImpl implements MessageDAO {
+
     /**
      * 添加消息，内部生成id，返回消息id
      *
      * @param messageBean
-     * @return int
+     * @return messageId
      * @throws SQLException
      */
     @Override
-    public int addMessage(MessageBean messageBean) throws SQLException {
-        return 0;
+    public Integer addMessage(MessageBean messageBean) throws SQLException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = "insert into ecollaborationweb.message (title,content,createTime," +
+                "senderId,readFlag,deadDate) values(?,?,?,?,?,?);";
+        try {
+            conn = DBUtils.getConnetction();
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, messageBean.getTitle());
+            ps.setString(2, messageBean.getContent());
+            ps.setString(3, messageBean.getCreateTime());
+            ps.setInt(4, messageBean.getSenderId());
+            ps.setInt(5, messageBean.getReadFlag());
+            ps.setString(6, messageBean.getDeadDate());
+            int i = ps.executeUpdate();
+            if (i != 0) {
+                sql = "SELECT LAST_INSERT_ID()";
+                ps = conn.prepareStatement(sql);
+                rs = ps.executeQuery();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+            }
+            return null;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            DBUtils.close(null, ps, conn);
+        }
     }
+
+
+
+    /**
+     * 添加到 消息-收件人 的关系表中，
+     *
+     * @param messageId
+     * @param receiverIds
+     * @return boolean
+     * @throws SQLException
+     */
+    @Override
+    public boolean addMessageReceiver(int messageId, List<Integer> receiverIds) throws SQLException {
+        boolean flag = true;
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = "insert into ecollaborationweb.message_receiver (messageId, receiverId, readFlag) values(?,?,?);";
+        try {
+            conn = DBUtils.getConnetction();
+            for (int i = 0; i < receiverIds.size(); i++) {
+                ps = conn.prepareStatement(sql);
+                ps.setInt(1, messageId);
+                ps.setInt(2, receiverIds.get(i));
+                ps.setInt(3, 0);
+                if (ps.executeUpdate() == 0) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtils.close(rs, ps, conn);
+        }
+        return flag;
+    }
+
 
     /**
      * 获取MessageBean，通过messageId
@@ -31,18 +105,6 @@ public class MessageDAOImpl implements MessageDAO{
      */
     @Override
     public MessageBean getMessageInfo(int messageId) throws SQLException {
-        return null;
-    }
-
-    /**
-     * 删除消息，获取MessageBean，通过messageId
-     *
-     * @param messageId
-     * @return MessageBean
-     * @throws SQLException
-     */
-    @Override
-    public MessageBean deleteByMessageId(int messageId) throws SQLException {
         return null;
     }
 
@@ -66,7 +128,7 @@ public class MessageDAOImpl implements MessageDAO{
      * @throws SQLException
      */
     @Override
-    public int getSenderIdByMessageId(int messageId) throws SQLException {
+    public Integer getSenderIdByMessageId(int messageId) throws SQLException {
         return 0;
     }
 
