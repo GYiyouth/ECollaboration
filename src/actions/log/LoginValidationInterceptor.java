cc7d49820b27f3dcb8c525b42c836fd686e0e5ec
GengRui <gr2016@mail.ustc.edu.cn>
2016-12-24 12:41:22 +0800

    登录拦截功能

---
+++ src/actions/log/LoginValidationInterceptor.java
@@ -0,0 +1,32 @@
+package actions.log;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+import java.util.Map;
+
+/**
+ * Created by GR on 2016/12/24.
+ */
+public class LoginValidationInterceptor extends AbstractInterceptor{
+
+    @Override
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        System.out.println("登录拦截器");
+        Map<String, Object> session= actionInvocation.getInvocationContext().getSession();
+        String userName = (String) session.get("userName");
+        System.out.println("已经登录的用户："+userName+"!");
+        if(LogAction.class==actionInvocation.getAction().getClass())
+        {
+            System.out.println("当前进行登录呢。。");
+            return actionInvocation.invoke();
+        }
+        if (userName == null) {
+            //终止执行，返回登录页面
+            return "login";
+        } else {
+            //继续执行剩余的拦截器和Action
+            return actionInvocation.invoke();
+        }
+    }
+}
