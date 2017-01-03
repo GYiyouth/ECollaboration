d83610c3f812cdad78c7945d4425ba16e73a2110
GengRui <gr2016@mail.ustc.edu.cn>
2017-01-03 20:53:19 +0800

    web端获取团队的信息

--- src/actions/team/GetTeamInfoAction.java
+++ src/actions/team/GetTeamInfoAction.java
@@ -1,6 +1,13 @@
 package actions.team;
 
+import DAO.projectDAO.ProjectDAO;
+import DAO.projectDAO.ProjectDAOImpl;
+import DAO.studentDAO.StudentDAO;
+import DAO.studentDAO.StudentDaoImpl;
+import DAO.teamDAO.TeamDAO;
 import DAO.teamDAO.TeamDAOImpl;
+import bean.domain.ProjectBean;
+import bean.domain.StudentBean;
 import bean.domain.TeamBean;
 import net.sf.json.JSONObject;
 import org.apache.struts2.interceptor.ServletRequestAware;
@@ -10,11 +17,13 @@ import org.apache.struts2.interceptor.SessionAware;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * 通过teamId获取team信息
  * 返回team信息：teamBean
+ * app端未传所有参数
  * Created by GR on 2017/1/3.
  */
 public class GetTeamInfoAction implements ServletRequestAware, ServletResponseAware,SessionAware {
@@ -27,6 +36,8 @@ public class GetTeamInfoAction implements ServletRequestAware, ServletResponseAw
 
     //action中生成
     private TeamBean teamBean;
+    ArrayList<StudentBean> studentBeans;
+    ArrayList<ProjectBean> projectBeans;
 
     @Override
     public void setServletRequest(HttpServletRequest request) {
@@ -49,12 +60,28 @@ public class GetTeamInfoAction implements ServletRequestAware, ServletResponseAw
     }
 
     public String getTeamInfoByTeamId(){
-        TeamDAOImpl teamDaoImpl = new TeamDAOImpl();
+        ArrayList<StudentBean> studentBeans = new ArrayList<>();
+        TeamDAO teamDao = new TeamDAOImpl();
+        StudentDAO studentDao = new StudentDaoImpl();
+        ProjectDAO projectDAO = new ProjectDAOImpl();
         TeamBean teamBean = new TeamBean();
         try{
-            teamBean = teamDaoImpl.getTeamInfo(teamId);
+            teamBean = teamDao.getTeamInfo(teamId);
             System.out.println(teamBean.getTeamName()+"团队名字<GetTeamInfoAction:getTeamInfoByTeamId>");
             setTeamBean(teamBean);
+            ArrayList<Integer> studentIds = studentDao.getStudentIdByTeamId(teamId);
+            for(int i=0;i<studentIds.size();i++){
+                StudentBean studentBean = studentDao.getInfoById(studentIds.get(i));
+                studentBeans.add(studentBean);
+            }
+            setStudentBeans(studentBeans);
+            ArrayList<ProjectBean> projectBeans = new ArrayList<>();
+            ArrayList<Integer> projectIds = projectDAO.getProjectIdListByTeamId(teamId);
+            for(int i=0;i<projectIds.size();i++){
+                ProjectBean projectBean = projectDAO.getProjectInfo(projectIds.get(i));
+                projectBeans.add(projectBean);
+            }
+            setProjectBeans(projectBeans);
         }catch(Exception e){
             e.printStackTrace();
             return "fail";
@@ -66,6 +93,8 @@ public class GetTeamInfoAction implements ServletRequestAware, ServletResponseAw
         JSONObject jsonObject = new JSONObject();
         if(getTeamInfoByTeamId().equals("success")){
             jsonObject.put("teamBean", getTeamBean());
+            jsonObject.put("studentBeans", getStudentBeans());
+            jsonObject.put("projectBeans", getProjectBeans());
             jsonObject.put("result", "success");
 
             this.response.setCharacterEncoding("UTF-8");
@@ -96,5 +125,21 @@ public class GetTeamInfoAction implements ServletRequestAware, ServletResponseAw
 
     public void setTeamBean(TeamBean teamBean) {
         this.teamBean = teamBean;
+    }
+
+    public ArrayList<StudentBean> getStudentBeans() {
+        return studentBeans;
+    }
+
+    public void setStudentBeans(ArrayList<StudentBean> studentBeans) {
+        this.studentBeans = studentBeans;
+    }
+
+    public ArrayList<ProjectBean> getProjectBeans() {
+        return projectBeans;
+    }
+
+    public void setProjectBeans(ArrayList<ProjectBean> projectBeans) {
+        this.projectBeans = projectBeans;
     }
 }
