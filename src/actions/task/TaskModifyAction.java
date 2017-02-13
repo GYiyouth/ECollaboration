37105c7a42fdfbff6ef869cbe9fc81f9f1f560b4
葛尧 <geyao2012@gmail.com>
2017-02-13 12:31:50 +0800

    TaskModifyAction完成json改造，未测试

--- src/actions/task/TaskModifyAction.java
+++ src/actions/task/TaskModifyAction.java
@@ -8,20 +8,27 @@ import DAO.teacherDAO.TeacherDAO;
 import DAO.teacherDAO.TeacherDAOImpl;
 import bean.domain.TaskBean;
 import bean.domain.UserBean;
+import net.sf.json.JSONObject;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
 import org.apache.struts2.interceptor.SessionAware;
+import smallTools.JSONHandler;
 import smallTools.Time;
 import smallTools.TimeImpl;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 /**
  * 任务修改的action
- * 提交的信息必须包含新的实施project数组
+ * 提交的信息必须包含新的实施projects数组
  * Created by geyao on 2016/12/25.
  */
-public class TaskModifyAction implements SessionAware {
+public class TaskModifyAction implements SessionAware, ServletRequestAware, ServletResponseAware {
 	private Map session;
 	private List<String> projects;
 	private String title;
@@ -29,17 +36,32 @@ public class TaskModifyAction implements SessionAware {
 	private String beginDate;
 	private String targetDate;
 	private int taskId;
+	private HttpServletResponse response;
+	private HttpServletRequest request;
 
-	public String execute() throws Exception {
-		if (projects == null || projects.size() < 1)
-			return "fail";
+	/**
+	 * 修改任务，会判断提交来的projects数组是否正确，仅对数据库中存在的项目添加任务
+	 * 返回 successProjectsId - 存在的项目ID 的ArrayList
+	 *     failProjectsId - 不存在的项目ID的 ArrayList
+	 *     taskBean - 服务器拿到并更新了的taskBean
+	 * @throws Exception
+	 */
+	public void execute() throws Exception {
+		JSONObject jsonObject = new JSONObject();
+		if (projects == null || projects.size() < 1){
+			JSONHandler.sendJSON(jsonObject, response);
+			return;
+		}
+
 		TeacherDAO teacherDAO = new TeacherDAOImpl();
 		ProjectDAO projectDAO = new ProjectDAOImpl();
 		TaskDAO taskDAO = new TaskDAOImpl();
 		UserBean userBean = (UserBean) session.get("userBean");
 		TaskBean taskBean = taskDAO.getTaskInfo(taskId);
-		if (taskBean == null)
-			return "fail";
+		if (taskBean == null){
+			JSONHandler.sendJSON(jsonObject, response);
+			return;
+		}
 		if (title != null && !title.equals(""))
 			taskBean.setTitle(title);
 		if (content != null && !content.equals(""))
@@ -48,23 +70,30 @@ public class TaskModifyAction implements SessionAware {
 			taskBean.setBeginDate(beginDate);
 		if (targetDate != null && !targetDate.equals(""))
 			taskBean.setTargetDate(targetDate);
-		ArrayList<Integer> projects = new ArrayList<>();
+		ArrayList<Integer> successProjects = new ArrayList<>();
+		ArrayList<Integer> failProjects = new ArrayList<>();
 		for (String str : this.projects) {
 			int i = Integer.parseInt(str);
 			if (projectDAO.getProjectInfo(i) != null)
-				projects.add(i);
-			else
-				return "fail";
+				successProjects.add(i);
+			else{
+				failProjects.add(i);
+			}
 		}
-		if (projects.size() > 0) {
+		if (successProjects.size() > 0) {
 			taskDAO.updateTask(taskBean);
 			projectDAO.deleteTask_Project(taskId);
-			for (int i : projects) {
+			for (int i : successProjects) {
 				projectDAO.setProjectTask(i, taskId);
 			}
-			return "success";
-		} else
-			return "fail";
+			jsonObject.put("result", "success");
+		} else{
+			jsonObject.put("result", "fail");
+		}
+		jsonObject.put("successProjectsId", successProjects);
+		jsonObject.put("failProjectsId", failProjects);
+		jsonObject.put("taskBean", taskBean);
+		JSONHandler.sendJSON(jsonObject, response);
 	}
 
 	private TaskBean taskBeanInit(int teacherId){
@@ -138,5 +167,20 @@ public class TaskModifyAction implements SessionAware {
 
 	public void setTaskId(int taskId) {
 		this.taskId = taskId;
+	}
+
+	@Override
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+		try {
+			this.request.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void setServletResponse(HttpServletResponse response) {
+		this.response = response;
 	}
 }
